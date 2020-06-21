3b33b346599741c494190bede96a1482213f2f19
Ekaterina <57844753+mirlek@users.noreply.github.com>
2020-06-21 17:27:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.book').click(function(){
+        $('#modal').addClass('show');
+    })
+}
+    );
+    $('.modal12__overlay').click(function(){
+        $('#modal').removeClass('show');
+    })
